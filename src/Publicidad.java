a873550a3bbb5e04dd601516a106003550770acf
Unknown <isaacgomez47@hotmail.com>
2017-10-07 08:49:03 -0500

    Publicidad

--- src/Publicidad.java
+++ src/Publicidad.java
@@ -1,5 +1,14 @@
 import java.awt.*;
 import javax.swing.*;
 public class Publicidad extends JPanel{
-
+	public JLabel imagen;
+	public Publicidad() {
+		  setLayout( new BorderLayout());
+	        JLabel imagen = new JLabel();
+	        ImageIcon icono = new ImageIcon( "img/econo.jpg" );
+	        imagen.setIcon( icono );
+	        imagen.setHorizontalAlignment( JLabel.CENTER );
+	        add( imagen, BorderLayout.CENTER );
+	        setBackground( Color.WHITE );
+    }
 }
